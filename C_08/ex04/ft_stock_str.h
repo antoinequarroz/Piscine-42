c61fb1d67c04af255cb986374523e9df17990722
Antoine Quarroz <aquarroz@student.42lausanne.ch>
2022-06-30 09:47:57 +0200

    Ajout de l'exercice 8

---
+++ C_08/ex04/ft_stock_str.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aquarroz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/29 10:48:01 by aquarroz          #+#    #+#             */
+/*   Updated: 2022/06/29 10:48:03 by aquarroz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STOCK_STR_H
+#define FT_STOCK_STR_H
+
+typedef struct s_stock_str
+{
+	int		size;
+	char 	*str;
+	char 	*copy;
+}			t_stock_str;
+
+#endif
---
+++ C_08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aquarroz <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/29 10:55:07 by aquarroz          #+#    #+#             */
+/*   Updated: 2022/06/29 10:55:08 by aquarroz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "ft_stock_str.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	dest = (char *)malloc((ft_strlen(src) + 1) * sizeof(char));
+	if (!dest)
+		return (0);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+struct	s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int					i;
+	struct s_stock_str	*array;
+
+	array = malloc((ac + 1) * sizeof(struct s_stock_str));
+	if(!array)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		array[i].size = ft_strlen(av[i]);
+		array[i].str = av[i];
+		array[i].copy = ft_strdup(av[i]);
+		i++;
+	}
+	array[i].str = 0;
+	array[i].size = 0;
+	array[i].copy = 0;
+	return (array);
+}
